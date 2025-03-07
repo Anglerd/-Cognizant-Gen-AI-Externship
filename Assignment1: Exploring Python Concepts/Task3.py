1d3d9985ab33f7a4115b1079a33c4c01d87e0a7a
Anglerd <t_poventud@hotmail.com>
2025-03-07 16:19:59 -0500

    Create Task3.py

---
+++ Assignment1: Exploring Python Concepts/Task3.py
@@ -0,0 +1,11 @@
+# Task 3 - Conditional Statements
+# Ask the user to enter a number
+number = float(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if number > 0:
+    print("This number is positive. Awesome!")
+elif number < 0:
+    print("This number is negative. Better luck next time!")
+else:
+    print("Zero it is. A perfect balance!")
