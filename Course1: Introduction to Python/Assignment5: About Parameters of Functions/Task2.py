98c7c189fc5acc0eb4cd6300f71df64e1555d604
Anglerd <t_poventud@hotmail.com>
2025-03-07 17:10:57 -0500

    Create Task2.py

---
+++ Course1: Introduction to Python/Assignment5: About Parameters of Functions/Task2.py
@@ -0,0 +1,7 @@
+# Task 2 - Using Default Parameters
+def describe_pet(pet_name, animal_type="dog"):
+    print(f"I have a {animal_type} named {pet_name}.")
+
+# Example usage
+describe_pet("Buddy")
+describe_pet("Whiskers", "cat")
