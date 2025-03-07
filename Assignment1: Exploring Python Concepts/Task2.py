3527fdb3db84a6030b0def69c957b02d730e9945
Anglerd <t_poventud@hotmail.com>
2025-03-07 16:19:27 -0500

    Create Task2.py

---
+++ Assignment1: Exploring Python Concepts/Task2.py
@@ -0,0 +1,19 @@
+# Task 2 - Operators
+num1 = 10
+num2 = 3
+
+# Addition
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}. Math is fun, isn't it? 😄")
+
+# Subtraction
+difference = num1 - num2
+print(f"The difference between {num1} and {num2} is {difference}. Subtraction is just addition in reverse!")
+
+# Multiplication
+product = num1 * num2
+print(f"The product of {num1} and {num2} is {product}. Multiplying numbers is like magic!")
+
+# Division
+quotient = num1 / num2
+print(f"The division of {num1} by {num2} is {quotient:.2f}. Division is where things get interesting!")
